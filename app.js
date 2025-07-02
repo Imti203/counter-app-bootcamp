ae6f8e8285332c4b2753b5a19dafdb2e3fd92a68
Imtiaz Uddin <imtiazshakil203@gmail.com>
2025-07-02 17:30:54 +0600

    adding increment button functionality

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 // selector 
+const bodyElement = document.getElementById('body')
 const counterElement = document.getElementById('counter')
 const emojiElement = document.getElementById('emoji')
-const incrementElement = document.getElementById('increment')
-const decrementElement = document.getElementById('decrement')
-const resetElement = document.getElementById('reset')
+const incrementBtnElement = document.getElementById('increment')
+const decrementBtnElement = document.getElementById('decrement')
+const resetBtnElement = document.getElementById('reset')
+
+let count = 0 
+
+incrementBtnElement.addEventListener('click', () => {
+    count++
+    // update count update 
+    counterElement.textContent = count 
+    if(count > 0) {
+        // update emoji 
+        emojiElement.textContent = '😀'
+        // background color change 
+        body.style.backgroundColor = `hsl(120,${Math.min(100, count * 10)}%, 80%)`
+    }
+})
