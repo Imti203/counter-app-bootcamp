d85ecdc3849b03538570129a3bd2273af2ab46b3
Imtiaz Uddin <imtiazshakil203@gmail.com>
2025-07-02 16:40:49 +0600

    adding selector

---
+++ app.js
@@ -0,0 +1,6 @@
+// selector 
+const counterElement = document.getElementById('counter')
+const emojiElement = document.getElementById('emoji')
+const incrementElement = document.getElementById('increment')
+const decrementElement = document.getElementById('decrement')
+const resetElement = document.getElementById('reset')
